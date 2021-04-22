6dbffca114cfee894b288da91aaf6417e797d081
josephyan0816 <387717545@qq.com>
2021-04-22 12:10:58 +0800

    dajianxiangmu

---
+++ src/router/index.js
@@ -0,0 +1,45 @@
+ import {createApp} from 'vue'
+ import VueRouter from 'vue-router'
+
+const Home = () => import('../views/home/Home')
+const Category = () => import('../views/category/Category')
+const Cart = () => import('../views/cart/Cart')
+const Profile = () => import('../views/profile/Profile')
+
+// 1.安装插件
+//Vue.use(VueRouter)
+
+ createApp({}).use(router).mount('#app')
+
+// 2.创建router
+const routes = [
+  {
+    path: '',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    component: Home
+  },
+  {
+    path: '/category',
+    component: Category
+  },
+  {
+    path: '/cart',
+    component: Cart
+  },
+  {
+    path: '/profile',
+    component: Profile
+  }
+]
+
+
+const router=new VueRouter({
+   routes,
+   mode:'history'
+ })
+
+
+export default router
